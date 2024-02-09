6e3d0f27fd6820fe2bee98bc6b98aeb509c1ef2a
HasnatHridoy <125564213+HasnatHridoy@users.noreply.github.com>
2024-02-09 17:02:44 +0000

    Changes to be committed: 	modified:   Main.py 	new file:   fun.py
     Changes not staged for commit:
    	modified:   functionality.py

--- Main.py
+++ Main.py
@@ -4,12 +4,23 @@ import streamlit as st
 from streamlit.logger import get_logger
 import cv2
 from ultralytics import YOLO
-from functionality import image_pro, measurement, mainloop
+import fun
+import numpy as np
 
 model_path = "/home/vscode/.local/cattle-weight-ai/models/ob.pt"
 model_path2 = "/home/vscode/.local/cattle-weight-ai/models/sbest.pt"
 
 def run():
+
+    try:
+        model_ob = YOLO(model_path)
+        model_sg = YOLO(model_path2)
+    except Exception as ex:
+        st.error(
+            f"Unable to load model. Check the specified path: {model_path}")
+        st.error(ex)
+
+
     st.set_page_config(
         page_title="Welcome to AI powered Cattle weight detection.",
         page_icon="🪄",
@@ -27,8 +38,12 @@ def run():
         """
     )
 
+    
+
+
     source_img = st.file_uploader(
         "Upload an image...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
+    
     
     col1, col2 = st.columns(2)
 
@@ -41,14 +56,39 @@ def run():
                     caption="Uploaded Image",
                     use_column_width=True
                     )
+            
+            if st.button("Predict", type="primary"):
+
+                try:
+                    image_np = np.array(uploaded_image)
+
+                    result_ob = model_ob.predict(image_np)
+                    result_sg = model_sg.predict(image_np)
+
+                    weight, img, checker = fun.mainloop(image_np, result_sg, result_ob)
+                    transformed_img = PIL.Image.fromarray(img)
+
+                    with col2:
+                        st.image(transformed_img,
+                                caption='Detected Image',
+                                use_column_width=True
+                                )
+
+                    st.write(f'The weight is = {weight}')
+
+                    
+
+
+                    
+
+                except Exception as ex:
+                    st.error(
+                        f"Unable to image to array.")
+                    st.error(ex)
+
+                
     
-    try:
-        model_ob = YOLO(model_path)
-        model_sg = YOLO(model_path2)
-    except Exception as ex:
-        st.error(
-            f"Unable to load model. Check the specified path: {model_path}")
-        st.error(ex)
+        
 
 
 
